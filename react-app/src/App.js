bd77a4ea8c7b9a41890e678df9026d888cad4a18
ArchambaultP <ehras@hotmail.com>
2019-01-26 23:15:25 -0500

    setup

---
+++ react-app/src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './App.css';
+import HeaderComponent from './HeaderComponent';
+import {
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom'
+import SignupComponent from './SignupComponent';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+      <div className="App">
+        <HeaderComponent />
+      <Route path="/login" component={SignupComponent} />
+      </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
---
+++ react-app/src/HeaderComponent.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { NavItem, Navbar, Nav } from "react-bootstrap";
+import { BrowserRouter as Router, Link } from "react-router-dom";
+
+class HeaderComponent extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      signup: false
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <Navbar inverse collapseOnSelect>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="#brand">Fruit Trader</a>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav pullRight>
+              <NavItem>
+                <Link to="/login">Log In</Link>
+              </NavItem>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+export default HeaderComponent;
