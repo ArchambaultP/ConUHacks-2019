5bfcfc1b60e69e9b230ed52e660abc82fa4f4fc2
Radhep <Radhep.Saba@gmail.com>
2019-01-27 02:15:13 -0500

    local

--- react-app/src/ListingComponent.jsx
+++ react-app/src/ListingComponent.jsx
@@ -1,21 +1,45 @@
 import React from 'react'
 import axios from 'axios'
-
+import { ListGroup, ListGroupItem, View, Button } from "react-bootstrap";
 
 class ListComponent extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: null
+    };
+  }
   
-  componentDidMount() {
+  componentWillMount() {
     this.test();
   }
   test() {
     axios.get('http://localhost:8000/getProduce').then((result) => {
-      console.log(result);
+      // console.log(result.data);
+      // this.setState({data: result.data});
+
+      var arr = [];
+      for (var i = 0; i < result.data.length; i++) {
+        console.log(result.data[i]);
+        arr.push(result.data[i].name);
+      }
+      this.setState({data: arr});
+      console.log(arr);
+      
     })
   }
     render() {
+      if (!this.state.data) {
+        return <div />
+      }
+
       return (
-        <div>
-        <h1>Hello World</h1>
+        <div> 
+          <ListGroup>
+           {this.state.data.map(r => <ListGroupItem>{r}</ListGroupItem>)}     
+          </ListGroup>;  
         </div>
       );
     }
